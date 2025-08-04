550333367928fdacb48194877f11f09692efc613
Armanhhew <arman515551@gmail.com>
2025-08-04 03:57:29 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
-from telethon.sync import TelegramClient
-from sys import argv
+from pyrogram import Client, filters
+import os
 
-API_ID = 123456  # مقدار واقعی
-API_HASH = 'your_api_hash'
+api_id = int(os.environ.get("API_ID"))
+api_hash = os.environ.get("API_HASH")
 
-b = TelegramClient('ok', API_ID, API_HASH).start()
+# آیدی عددی کاربرانی که باید پیامشون ذخیره بشه
+target_user_ids = 1992411831  # ← جایگزین کن
 
-mess = b.get_messages(argv[1], limit=int(argv[2]))
-for i in mess[-1].reply_markup.rows:
-    for n in i.buttons:
-        print(n.text, '=', n.data)
-    print()
+app = Client("bot", api_id=api_id, api_hash=api_hash)
 
-b.disconnect()
+
+@app.on_message(filters.private)
+async def save_user_message(client, message):
+    sender_id = message.from_user.id
+
+    if sender_id in target_user_ids:
+        with open("saved_messages.txt", "a", encoding="utf-8") as file:
+            file.write(f"[{sender_id}] {message.text or '[non-text message]'}\n")
+        print(f"Saved message from {sender_id}")
+
+
+app.run()
