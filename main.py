ea7e9f2989c881b0cce416fc02b686f550403619
Armanhhew <arman515551@gmail.com>
2025-08-04 04:13:31 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
 from pyrogram import Client, filters
 import os
 
-api_id = int(os.environ.get("API_ID"))
-api_hash = os.environ.get("API_HASH")
+api_id = 11991770
+api_hash = "e0e93d0f4ff01ceed8006d7a0664848b"
 
-# آیدی عددی کاربرانی که باید پیامشون ذخیره بشه
-target_user_ids = 1992411831  # ← جایگزین کن
+target_user_ids = [1992411831]
 
-app = Client("bot", api_id=api_id, api_hash=api_hash)
+channel_username = "@jvjgjvju"  # یوزرنیم کانالت رو اینجا بزن
+
+app = Client("logger_bot", api_id=api_id, api_hash=api_hash)
 
 
 @app.on_message(filters.private)
-async def save_user_message(client, message):
+async def handler(client, message):
     sender_id = message.from_user.id
-
     if sender_id in target_user_ids:
-        with open("saved_messages.txt", "a", encoding="utf-8") as file:
-            file.write(f"[{sender_id}] {message.text or '[non-text message]'}\n")
-        print(f"Saved message from {sender_id}")
+        text = message.text or "[non-text message]"
+        await app.send_message(channel_username, f"پیام از {sender_id}:\n\n{text}")
 
 
 app.run()
